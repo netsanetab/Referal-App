0847e29b16b6eb77512865c3f4d8e7dce1ecd15d
Netsanet <free2.abayneh@gmail.com>
2021-03-13 14:56:19 +0300

    test msg

--- Referal/views.py
+++ Referal/views.py
@@ -48,7 +48,7 @@ def liaizon_signup_view(request):
         liaizonForm = forms.LiaizonForm(request.POST, request.FILES)
         if userForm.is_valid() and liaizonForm.is_valid():
             user = userForm.save()
-            user.set_password(user.password)
+            user.set_password(user.password) 
             user.save()
              liaizon = liaizonForm.save(commit=False)
             liaizon.user = user
